a6a7e0482b58496222f37e8083b73dfe3fa552c2
Keith <keith@kaelynskbookpro.lan>
2022-01-24 13:44:06 -0800

    styled more, working on dymamic rendering of categories, pushing for santo to work on components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import AppNavbar from './Navbar/Navbar.jsx';
-import Search from './Search.jsx';
+import  React  from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
-import Categories from './Categories/Categories.jsx';
 import WorkoutLog from './pages/WorkoutLog/workoutLog.jsx';
 import WorkoutComponent from './pages/WorkoutComponent/WorkoutComponent.jsx';
+import Home from './pages/Home.jsx';
+import axios from 'axios';
 
 
 
 //when home page is rendered, make an axios get req to the api to dynamically render workout categories. youll need a context 
 
-const App = () => (
+function App()  {
+  //use effect is a hook that runs a function once a component is rendered
+
+  //in this case you want to call  a function that makes a get request to the api for workout categories, using another hook to save the response data to pass down as props to the categories component to be dynamically rendered
+  // useEffect(() => {
+
+  // , []);
+  
+  return(
   <div>
     
-    <h1>PUMPED</h1>
-    <AppNavbar />
+    <h1>PUMP</h1>
+    
         <Routes>
           <Route path='/workout-log' component={WorkoutLog}/>
           {/* <Route path='/my-workout-plans' component={WorkoutPlan}/>
@@ -27,8 +35,7 @@ const App = () => (
           <Route path='/legs' component={Workouts}/>
           <Route path='/workouts' component={Workouts}/> */}
         </Routes>
-    <Search />
-    <Categories />
+          <Home />
 
       <div>
           <WorkoutComponent />
@@ -36,4 +43,5 @@ const App = () => (
     </div>
   
 );
+        };
 export default App;
