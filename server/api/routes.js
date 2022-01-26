166d8887a47d6d2f71b241827f857fab3b47b325
Zack Izdepski <zizdep1@gmail.com>
2022-01-26 11:16:46 -0600

    Note: Added some pseudocode and deleted a console log

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
+const axios = require('axios');
 const { signup, login, deleteUser } = require('./user');
 const { getWorkoutPlan } = require('./workoutPlan');
 const { addWorkout, getWorkoutsByCategory } = require('./workout');
 const { addToUserLog } = require('./userLog');
-const axios = require('axios');
 
 const pumpedRouter = Router();
 
@@ -21,9 +21,9 @@ pumpedRouter.post('/addToUserLog', addToUserLog);
 
 pumpedRouter.delete('/deleteUser/:username', deleteUser);
 
-pumpedRouter.get('/workoutsByCategory', async(req, res) => {
-  const result = await axios.get('https://wger.de/api/v2/exercise/?language=2&limit=100')
-  console.log(result.data.results);
+// This endpoint makes a request to the api for exercise data in english with a limit of 100 items.
+pumpedRouter.get('/workoutsByCategory', async (req, res) => {
+  const result = await axios.get('https://wger.de/api/v2/exercise/?language=2&limit=100');
   res.send(result.data.results);
 });
 
