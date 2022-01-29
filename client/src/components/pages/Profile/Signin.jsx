e5f3f33140821f127076ebeee887ae2fe1404cd4
Santolococo <santolococo@gmail.com>
2022-01-29 10:01:46 -0700

    removed console logs in many places

---
+++ client/src/components/pages/Profile/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signin = () => {
+  return <div>
+    <a href='http://localhost:3000/google'>
+      Signin With Google
+    </a>
+  </div>;
+};
+
+export default Signin;
